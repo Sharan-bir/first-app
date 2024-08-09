e027e7d18f1dd82b2232109e4fc7289eeef40839
sharanbir <birsharan105@gmail.com>
2024-08-09 17:12:59 +0530

    text_1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sharan</Text>
+      <Text>Divya</Text>
       <StatusBar style="auto" />
     </View>
   );
